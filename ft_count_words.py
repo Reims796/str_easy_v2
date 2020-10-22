8375b1f5941aff9e1348ccad9a457422b65ab5a6
Reims796 <71494978+Reims796@users.noreply.github.com>
2020-10-22 12:39:11 +0300

    Update ft_count_words.py

--- ft_count_words.py
+++ ft_count_words.py
@@ -1,24 +1,25 @@
-def ft_len(str):
-    a = 0
-    for i in str:
-        a += 1
-    return (a)
+def ft_len(a):
+    b = 0
+    for i in a:
+        b += 1
+    return (b)
 
 
-def ft_count_words(str):
-    k = 0
-    d = ft_len(str)
+def ft_count_words(a):
+    x = 0
+    d = ft_len(a)
     i = 0
-    if str[0] == ' ':
+    if a[0] == ' ':
         while i < d:
-            if str[i] == ' ' and str[i - 1] != ' ':
-                k = k + 1
+            if a[i] == ' ' and a[i - 1] != ' ':
+                x = x + 1
             i = i + 1
-        return k
+        return x
     else:
         while i < d:
-            if str[i] == ' ' and str[i - 1] != ' ':
-                k = k + 1
+            if a[i] == ' ' and a[i - 1] != ' ':
+                x = x + 1
             i = i + 1
-        return k + 1
+        return x + 1
+
 
